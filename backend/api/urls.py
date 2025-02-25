dbc56f8cd6fe4011b6b82d00cf6b55acdc863f7f
MGH7777 <meron.ghirmay@yahoo.com>
2025-02-25 16:48:08 +0100

    Update urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,1 +1,7 @@
+from django.urls import path
+from . import views  # Import views from the current folder
 
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+]
