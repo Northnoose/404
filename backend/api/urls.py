e9bc28ee42830768a7ca83b9c0ed793c8740d7fe
Steffen <Steffen.Nordnes@hotmail.com>
2025-02-26 15:57:12 +0100

    La til ekstra kode i urls.py og login + registrer templatene

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,4 +4,7 @@ from . import views  # Import views from the current folder
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('login/', views.login_view, name='login'),
+    path('logout', views.logout_view, name='logout'),
+    path('reigster', views.register_view, name='register')
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
     return render(request, 'home.html')
-
+    
 def about(request):
     return render(request, 'about.html')
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        
+        user = authenticate(request, username = username, password = password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+    return render(request, "login.html")
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+def register_view(request):
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    return render(request,'register.html', { 'form': form})
