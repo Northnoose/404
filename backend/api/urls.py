c92d3f67e6d0951edf4f0d3c8d460e7acdd606f7
MGH7777 <meron.ghirmay@yahoo.com>
2025-02-26 19:44:45 +0100

    lagt et par nye linjer som er nødvendig i utviklingen for at Django skal kunne servere statiske filer

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from . import views  # Import views from the current folder
+from django.conf import settings
+from django.conf.urls.static import static  # Import static to serve static files
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('login/', views.login_view, name='login'),
-    path('logout', views.logout_view, name='logout'),
-    path('reigster', views.register_view, name='register')
+    path('logout/', views.logout_view, name='logout'),  # Add slash here
+    path('register/', views.register_view, name='register'),  # Add slash here
 ]
 
-# Nødvendig i utvikling for at Django skal kunne servere statiske filer (som CSS, JS, og bilder) direkte fra STATIC_URL-katalogen.
+# Serve static files during development
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
