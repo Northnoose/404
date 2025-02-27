2a35473ed6e66035fb91f1e3d3ce6815c0c3b23e
micalmussie <165506663+micalmussie@users.noreply.github.com>
2025-02-27 21:50:49 +0100

    Update urls.py
    to nye url-er brukerprofil og Rediger profil

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views  # Import views from the current folder
 from django.conf import settings
 from django.conf.urls.static import static  # Import static to serve static files
+from django.urls import path
+from .views import profile, edit_profile
+
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -13,3 +16,11 @@ urlpatterns = [
 
 # Serve static files during development
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+
+urlpatterns += [
+    path('profile/', profile, name='profile'),
+    path('profile/edit/', edit_profile, name='edit_profile'),
+]
+
