0b3c7cff192227c8dbb9fe054f724bf717979a1b
Sebastian <sebb95@hotmail.com>
2025-03-06 15:17:11 +0100

    Fylt ut Serializers.py

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,6 @@
 
 from rest_framework import serializers
-from .models import User, UserPermission
+from .models import User, UserPermission, Task, Module, UserTask, UserModule
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta():
@@ -12,3 +12,32 @@ class UserPermissionSerializer(serializers.ModelSerializer):
         model = UserPermission
         fields = '__all__'
         
+        
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ['id', 'description', 'task_object']
+
+
+class ModuleSerializer(serializers.ModelSerializer):
+    task = TaskSerializer()  
+
+    class Meta:
+        model = Module
+        fields = ['id', 'task', 'description']
+
+class UserTaskSerializer(serializers.ModelSerializer):
+    user = UserSerializer()  
+    task = TaskSerializer()
+    class Meta:
+        model = UserTask
+        fields = ['user', 'task', 'score', 'finished']
+
+
+
+class UserModuleSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    module = ModuleSerializer() 
+    class Meta:
+        model = UserModule
+        fields = ['user', 'module', 'score', 'finished']
